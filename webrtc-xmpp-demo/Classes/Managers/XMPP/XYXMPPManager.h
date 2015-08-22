07123ef4c707c1a0786cc7cb57ef108899deccdd
RussellXY <419582116@qq.com>
2015-08-22 23:20:39 +0800

    项目搭建

---
+++ webrtc-xmpp-demo/Classes/Managers/XMPP/XYXMPPManager.h
@@ -0,0 +1,13 @@
+//
+//  XYXMPPManager.h
+//  webrtc-xmpp-demo
+//
+//  Created by Russell.Y on 15/8/22.
+//  Copyright (c) 2015年 RussellY. All rights reserved.
+//
+
+#import <Foundation/Foundation.h>
+
+@interface XYXMPPManager : NSObject
+
+@end
---
+++ webrtc-xmpp-demo/Classes/Managers/XMPP/XYXMPPManager.m
@@ -0,0 +1,13 @@
+//
+//  XYXMPPManager.m
+//  webrtc-xmpp-demo
+//
+//  Created by Russell.Y on 15/8/22.
+//  Copyright (c) 2015年 RussellY. All rights reserved.
+//
+
+#import "XYXMPPManager.h"
+
+@implementation XYXMPPManager
+
+@end

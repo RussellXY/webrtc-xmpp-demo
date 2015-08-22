161b826aeb8bcd715f7228004f0a29e41c5a81ff
RussellXY <419582116@qq.com>
2015-08-22 21:45:18 +0800

    Initial Commit

---
+++ webrtc-xmpp-demo/AppDelegate.h
@@ -0,0 +1,17 @@
+//
+//  AppDelegate.h
+//  webrtc-xmpp-demo
+//
+//  Created by Russell.Y on 15/8/22.
+//  Copyright (c) 2015年 RussellY. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+
+@interface AppDelegate : UIResponder <UIApplicationDelegate>
+
+@property (strong, nonatomic) UIWindow *window;
+
+
+@end
+
---
+++ webrtc-xmpp-demo/ViewController.h
@@ -0,0 +1,15 @@
+//
+//  ViewController.h
+//  webrtc-xmpp-demo
+//
+//  Created by Russell.Y on 15/8/22.
+//  Copyright (c) 2015年 RussellY. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+
+@interface ViewController : UIViewController
+
+
+@end
+
